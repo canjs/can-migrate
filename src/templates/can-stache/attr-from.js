804cd295ad838b352d0814f1673a1d5248a52daf
Kevin Phillips <kevin@bitovi.com>
2018-03-06 17:27:34 -0600

    Fixing false positives with attr-from codemod
    This change restricts the attr-from codemod to attributes:
    
    * on custom elements
    * that don't start with `can-`
    * that are not `aria-` or `data-` attributes
    * and are in the `globalAttributes` list

--- src/templates/can-stache/attr-from.js
+++ src/templates/can-stache/attr-from.js
@@ -1,3 +1,4 @@
+import parser from 'can-view-parser';
 
 const globalAttributes = ['xml:lang', 'xml:base', 'onabort', 'onautocomplete',
   'onautocompleteerror', 'onblur', 'oncancel', 'oncanplay', 'oncanplaythrough',
@@ -14,25 +15,70 @@ const globalAttributes = ['xml:lang', 'xml:base', 'onabort', 'onautocomplete',
   'contextmenu', 'dir', 'draggable', 'hidden', 'id', 'is', 'lang', 'style',
   'tabindex', 'title'];
 
+const noop = function() {};
+
+const isDataAttribute = (attributeName) => attributeName.indexOf('data-') === 0;
+const isAriaAttribute = (attributeName) => attributeName.indexOf('aria-') === 0;
+const isInGlobalAttributes = (attributeName) => globalAttributes.indexOf(attributeName) >= 0;
+const attributeShouldBeModified = (attributeName) =>
+  !isDataAttribute(attributeName) &&
+  !isAriaAttribute(attributeName) &&
+  !isInGlobalAttributes(attributeName);
+
+const isCustomElement = (tagName) => tagName.includes('-');
+const isCanElement = (tagName) => tagName.indexOf('can-') === 0;
+const tagShouldBeModified = (tagName) =>
+  isCustomElement(tagName) &&
+  !isCanElement(tagName);
+
 export default function transformer(file) {
   let src = file.source;
-  var path = file.path;
-  var type = path.slice(path.lastIndexOf('.') + 1);
+  const path = file.path;
+  const type = path.slice(path.lastIndexOf('.') + 1);
 
   if (type === 'stache') {
+    // this list contains a boolean for each attribute found during parsing to indicate
+    // whether its value should be modified when looping through attributes with regex
+    let attributeAtIndexShouldBeModified = [];
+
+    let currentTag = '';
+    let currentAttr = '';
+
+    parser(src, {
+      start: function(tagName) {
+        currentTag = tagName;
+      },
+      end: function() {
+        currentTag = '';
+      },
+      attrStart: function(attrName) {
+        currentAttr = attrName;
+      },
+      attrEnd: function() {
+        currentAttr = '';
+      },
+      attrValue: function() {
+        const shouldModify = tagShouldBeModified(currentTag) &&
+          attributeShouldBeModified(currentAttr);
+        attributeAtIndexShouldBeModified.push(shouldModify);
+      },
+      done: noop,
+      special: noop
+    });
+
     const alphaNumeric = 'A-Za-z0-9';
-    const alphaNumericHU = '-:_'+alphaNumeric;
-    const attributeRegexp = new RegExp("["+alphaNumericHU+"]+\s*=\s*(\"[^\"]*\"|'[^']*')", 'gm'); // jshint ignore:line
+    const alphaNumericHU = '-:_' + alphaNumeric;
+    const attributeRegexp = new RegExp('[' + alphaNumericHU + ']+\s*=\s*("[^"]*"|\'[^\']*\')', 'gm');
     const attributes = src.match(attributeRegexp);
+
     for (let i = 0; i < attributes.length; i++) {
       const attribute = attributes[i].slice(0, attributes[i].lastIndexOf('='));
       const value = attributes[i].slice(attributes[i].lastIndexOf('=') + 1);
-      if (globalAttributes.indexOf(attribute) === -1 && !attribute.includes('aria') && !attribute.includes('data')) {
+      if (attributeAtIndexShouldBeModified[i]) {
         src = src.replace(attribute, attribute + ':from');
         if (value.includes("'")) { // jshint ignore:line
           src = src.replace(value, '"' + value + '"'); // jshint ignore:line
-        }
-        else {
+        } else {
           src = src.replace(value, "'" + value + "'"); // jshint ignore:line
         }
       }
