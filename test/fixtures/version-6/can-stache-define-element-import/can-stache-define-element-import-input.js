159d79c50adbbae2862456aebe7cc58f309fc8d4
Matt Chaffe <matt@mattchaffe.uk>
2019-06-27 12:58:07 +0100

    update tests to check better imports

--- test/fixtures/version-6/can-stache-define-element-import/can-stache-define-element-import-input.js
+++ test/fixtures/version-6/can-stache-define-element-import/can-stache-define-element-import-input.js
@@ -1,3 +1,4 @@
 import Component from 'can-component';
 import { Component, DefineMap } from 'can';
 import { Component } from 'can';
+import { Component, DefineMap, DefineList, fixture, realtimeRestModel, route, domEvents, enterEvent } from '//unpkg.com/can@5/everything.mjs';
--- test/fixtures/version-6/can-stache-define-element-import/can-stache-define-element-import-output.js
+++ test/fixtures/version-6/can-stache-define-element-import/can-stache-define-element-import-output.js
@@ -1,3 +1,4 @@
 import StacheDefineElement from 'can-stache-define-element';
 import { StacheDefineElement, DefineMap } from 'can';
 import { StacheDefineElement } from 'can';
+import { StacheDefineElement, DefineMap, DefineList, fixture, realtimeRestModel, route, domEvents, enterEvent } from '//unpkg.com/can@5/everything.mjs';
