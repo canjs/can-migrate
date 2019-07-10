83f2527095c3531f2ba5d365d7f55d5d0bf3032d
Matt Chaffe <matt@mattchaffe.uk>
2019-07-10 19:08:22 +0100

    transform new DefineList into DefineArray

--- src/templates/can-define-array/array-constructor.js
+++ src/templates/can-define-array/array-constructor.js
@@ -1,6 +1,8 @@
 import makeDebug from 'debug';
 import fileTransform from '../../../utils/fileUtil';
 
+const replacementName = 'DefineArray';
+
 function transformer(file, api) {
   const debug = makeDebug(`can-migrate:can-define-array/array-constructor:${file.path}`);
   const j = api.jscodeshift;
@@ -8,29 +10,40 @@ function transformer(file, api) {
   // Transform different file types
   return fileTransform(file, function (source) {
     const root = j(source);
-  
-    return root
-      .find(j.NewExpression, {
-        callee: {
-          name: 'DefineArray'
-        }
-      })
-      .forEach(path => {
-        // Get the current arguments
-        const args = path.value.arguments;
-        if (args.length) {
-          // Replace the current new expression with a new one
-          // using the spread operator to spread the args
-          j(path).replaceWith(j.newExpression(
-            j.identifier('DefineArray'),
-            [j.spreadElement(...args)]
-          ));
 
-          debug('Replacing DefineArray([]) with DefineArray(...[])');
-        }
-      })
-      .toSource();
+    // Transform any `new DefineArray`
+    replaceWithSpread(j, root, replacementName, debug);
+    // Replace any `new DefineList` with `new DefineArray`
+    replaceWithSpread(j, root, 'DefineList', debug);
+    
+    return root.toSource();
   });
+}
+
+function replaceWithSpread(j, root, name, debug) {
+  root
+    .find(j.NewExpression, {
+      callee: {
+        name
+      }
+    })
+    .forEach(path => {
+      // Get the current arguments
+      const args = path.value.arguments;
+      if (args.length) {
+        // Replace the current new expression with a new one
+        // using the spread operator to spread the args
+        j(path).replaceWith(j.newExpression(
+          j.identifier(replacementName),
+          [j.spreadElement(...args)]
+        ));
+
+        debug(`Replacing ${replacementName}([]) with ${replacementName}(...[])`);
+      } else if (path.value.callee.name !== replacementName) {
+        // Update the name
+        path.value.callee.name = replacementName;
+      }
+    });
 }
 
 transformer.forceJavaScriptTransform = true;
--- test/fixtures/version-6/can-define-array/array-constructor-output.js
+++ test/fixtures/version-6/can-define-array/array-constructor-output.js
@@ -1,5 +1,7 @@
 const foo = new DefineArray(...[1, 2, 3]);
-
 const bar = new DefineArray();
-
 const zed = new DefineArray(...[]);
+
+const list = new DefineArray(...[]);
+const list1 = new DefineArray(...[1, 2]);
+const list2 = new DefineArray();
